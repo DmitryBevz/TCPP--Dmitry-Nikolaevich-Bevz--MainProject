e20ca7538f869dde008d0a7b18c2379bfb14d8ff
Dmitry <bevz00vn@ukr.net>
2021-02-28 19:07:58 +0200

    Comment (alpha)

--- ConsoleApp1_test/Program.cs
+++ ConsoleApp1_test/Program.cs
@@ -6,6 +6,7 @@ namespace ConsoleApp1_test
     {
         static void Main(string[] args)
         {
+            // edited from alpha branch
             Console.WriteLine("Hello World!");
         }
     }
